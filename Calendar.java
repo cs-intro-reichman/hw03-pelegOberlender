31ecef3ef9a60c635f0c97e693140a11019b9574
פלג אוברלנדר <pelegoberlender@h-MacBook-Air-sl-plg.local>
2024-01-09 11:07:19 +0200

    new

--- Calendar.java
+++ Calendar.java
@@ -8,18 +8,10 @@ public class Calendar {
 	static int year = 1900;
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
+	
 	public static void main(String args[]) {
-		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
-	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
-	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
 	    int debugDaysCounter = 0; 
 	    int givenYear = Integer.parseInt(args[0]);
-	    //// Write the necessary initialization code, and replace the condition
-	    //// of the while loop with the necessary condition 
 	 	while (year < givenYear) {	 
 	 		advance();		
 	 	}
@@ -33,13 +25,6 @@ public class Calendar {
 
 	 	}
 	 }	
-	 	
-	 	//// Write the necessary ending code here
-
-	
-	 // Advances the date (day, month, year) and the day-of-the-week.
-	 // If the month changes, sets the number of days in this month.
-	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
 	 private static void advance() {
 	 	if(dayOfMonth < nDaysInMonth(month,year)) {
 	 		dayOfMonth++;
